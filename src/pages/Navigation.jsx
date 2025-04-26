a884dc752782ea853fb33c534360f4606c54e684
Anayat Ali <anayatalifw@gmail.com>
2025-04-26 02:39:08 -0400

    fixed nav

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -17,13 +17,15 @@ const Navigation = ({ isOpen, onClose }) => {
     };
 
     return (
-        <nav className={`navigation ${isOpen ? 'open' : ''}`}>
+        <div>
             <img 
                 src={isOpen ? close : menu} 
                 className="menu-icon" 
                 onClick={onClose} 
                 alt={isOpen ? "Close menu" : "Open menu"}
             />
+        <nav className={`navigation ${isOpen ? 'open' : ''}`}>
+
             <ul>
                 <li><Link to="/homepage" onClick={onClose}>Home</Link></li>
                 {user ? (
@@ -31,7 +33,6 @@ const Navigation = ({ isOpen, onClose }) => {
                         <li><Link to="/leaderboard" onClick={onClose}>Leaderboard</Link></li>
                         <li><Link to="/notifications" onClick={onClose}>Notifications</Link></li>
                         <li><Link to="/redeem" onClick={onClose}>Redeem Points</Link></li>
-                        <li><Link to="/scanner" onClick={onClose}>Scan QR</Link></li>
                         <li><Link to="/profile" onClick={onClose}>Profile</Link></li>
                         {isAdmin && (
                             <li><Link to="/admin" onClick={onClose}>Admin Panel</Link></li>
@@ -50,6 +51,7 @@ const Navigation = ({ isOpen, onClose }) => {
                 )}
             </ul>
         </nav>
+        </div>
     );
 };
 
